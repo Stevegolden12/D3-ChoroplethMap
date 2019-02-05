adf198ad28d1f59991fd243938fc7c7ea5cc2e07
Steve Golden <sgolden2525@outlook.com>
2019-02-05 01:26:52 -0600

    Rework API
    Rework API to add multiple API's,
    
    Added d3.queue for mulitple API's and change code for queue.

--- js/index.js
+++ js/index.js
@@ -1,15 +1,52 @@
+/*
 Promise.all([
   fetch('https://raw.githubusercontent.com/no-stack-dub-sack/testable-projects-fcc/master/src/data/choropleth_map/for_user_education.json').then(value => value.json()),
   fetch("https://raw.githubusercontent.com/no-stack-dub-sack/testable-projects-fcc/master/src/data/choropleth_map/counties.json").then(value => value.json())
-])
+  ])
   .then((value) => {
-    console.log(value[0])
-    console.log(value[1])
-
-   
     //json response
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+    console.log(value)
+    })
+      .catch((err) => {
+        console.log(err);
+      });
+      */
 
+//Width and Height
+const width = 1000
+const height = 550
+
+const projection = d3.geoAlbers()
+  .scale(1000)
+  .translate([width / 2, height / 2])
+
+const path = d3.geoPath()
+  .projection(projection);
+
+const svg = d3.select("body")
+  .append("svg")
+  .attr("width", width)
+  .attr("height", height)
+
+// Queue up datasets using d3 Queue
+d3.queue()
+  .defer(d3.json, "https://raw.githubusercontent.com/no-stack-dub-sack/testable-projects-fcc/master/src/data/choropleth_map/for_user_education.json") //Load State and education information
+  .defer(d3.json, "https://raw.githubusercontent.com/no-stack-dub-sack/testable-projects-fcc/master/src/data/choropleth_map/counties.json") //LOad US Counties
+  .await(ready);
+
+// Ready Function, runs when data is loaded
+function ready(error, education, counties) {
+  if (error) throw error;
+
+  console.log(counties);
+  /*
+  svg.append("g")
+    .attr("class", "counties")
+    .selectAll("path")
+    .data(topojson.feature(us, us.objects.counties).features) // Bind TopoJSON data elements
+    .enter().append("path")
+    .attr("d", path)
+    .style("fill", "white")
+    .style("stroke", "black");
+    */
+}
