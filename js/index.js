7333c575690c74e63a27307e4060d4b26baad21a
Steve Golden <sgolden2525@outlook.com>
2019-02-05 20:04:19 -0600

    Process of svg
    Checking svg and making sure everything is correct

--- js/index.js
+++ js/index.js
@@ -1,52 +1,38 @@
-/*
-Promise.all([
-  fetch('https://raw.githubusercontent.com/no-stack-dub-sack/testable-projects-fcc/master/src/data/choropleth_map/for_user_education.json').then(value => value.json()),
-  fetch("https://raw.githubusercontent.com/no-stack-dub-sack/testable-projects-fcc/master/src/data/choropleth_map/counties.json").then(value => value.json())
-  ])
-  .then((value) => {
-    //json response
-    console.log(value)
-    })
-      .catch((err) => {
-        console.log(err);
-      });
-      */
 
-//Width and Height
-const width = 1000
-const height = 550
+var width = 720,
+  height = 500;
 
-const projection = d3.geoAlbers()
+var projection = d3.geoAlbers()
   .scale(1000)
-  .translate([width / 2, height / 2])
+  .translate([width / 2, height / 2]);
 
-const path = d3.geoPath()
+var path = d3.geoPath()
   .projection(projection);
 
-const svg = d3.select("body")
-  .append("svg")
+var svg = d3.select("#svgWrapper").append("svg")
   .attr("width", width)
-  .attr("height", height)
+  .attr("height", height);
 
 // Queue up datasets using d3 Queue
 d3.queue()
-  .defer(d3.json, "https://raw.githubusercontent.com/no-stack-dub-sack/testable-projects-fcc/master/src/data/choropleth_map/for_user_education.json") //Load State and education information
   .defer(d3.json, "https://raw.githubusercontent.com/no-stack-dub-sack/testable-projects-fcc/master/src/data/choropleth_map/counties.json") //LOad US Counties
+  .defer(d3.json, "https://raw.githubusercontent.com/no-stack-dub-sack/testable-projects-fcc/master/src/data/choropleth_map/for_user_education.json") //Load State and education information
   .await(ready);
 
 // Ready Function, runs when data is loaded
-function ready(error, education, counties) {
+function ready(error, us, education) {
   if (error) throw error;
 
-  console.log(counties);
-  /*
+
+  console.log(us);
+  
   svg.append("g")
     .attr("class", "counties")
     .selectAll("path")
-    .data(topojson.feature(us, us.objects.counties).features) // Bind TopoJSON data elements
+    .data(topojson.feature(us, us.objects.counties).features)
     .enter().append("path")
     .attr("d", path)
     .style("fill", "white")
     .style("stroke", "black");
-    */
+
 }
